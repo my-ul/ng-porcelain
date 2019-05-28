f8910121f1a3b65bac6b949e0f07317632925d2e
Brad Kovach <brad.kovach@ul.com>
2019-05-28 13:37:10 -0600

    remove old projects/porcelain project

--- projects/porcelain/src/lib/shared/animations/index.ts
+++
@@ -1,4 +0,0 @@
-// start:ng42.barrel
-export * from './blockInitialAnimation.trigger';
-export * from './slideInOut.trigger';
-// end:ng42.barrel
--- projects/porcelain/src/lib/shared/utilities/toDateString.ts
+++
@@ -1,5 +0,0 @@
-import * as _moment from 'moment';
-const moment = _moment;
-
-export const toDateString = (date: Date | _moment.Moment, format: 'YYYY-MM-DD') =>
-	moment(date).format(format);
