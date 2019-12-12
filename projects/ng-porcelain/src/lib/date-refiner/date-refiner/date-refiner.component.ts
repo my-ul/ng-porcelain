53680249242da793f98d2132057bb25b87639962
Brad Kovach <bradkovach@gmail.com>
2019-12-11 17:50:00 -0700

    Clean up branching logic to be more succinct. change version to 1.8.2

--- projects/ng-porcelain/src/lib/date-refiner/date-refiner/date-refiner.component.ts
+++ projects/ng-porcelain/src/lib/date-refiner/date-refiner/date-refiner.component.ts
@@ -176,17 +176,16 @@ export class DateRefinerComponent implements OnInit {
 	onChange(newOptionSlug: string) {
 		console.group('onChange(newOptionSlug)', { newOptionSlug });
 
-		if (this.currentOptionSlug !== newOptionSlug) {
+		if (
+			// the current option has changed to a predefined range
+			(this.currentOptionSlug !== newOptionSlug)
+			
+			// or the custom values have changed
+			|| (this.currentOptionSlug === 'custom' && newOptionSlug === 'custom')
+		) {
 			this.currentOptionSlug = newOptionSlug;
 			this.ignoreNext = true;
 			this.refiner.valueSubject.next(this.getValue());
-		}
-		//if inside custom date refiner i.e mydatepicker if value is changed
-		else if (this.currentOptionSlug === 'custom' && newOptionSlug === 'custom') {
-			this.currentOptionSlug = newOptionSlug;
-			this.ignoreNext = true;
-			this.refiner.valueSubject.next(this.getValue());
-		} else {
 		}
 
 		console.groupEnd();
