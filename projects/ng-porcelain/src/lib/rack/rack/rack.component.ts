12870ed6dc230cafb36755474d5cc958731c64a1
Rao <83346@global.ul.com>
2020-05-30 15:13:10 +0530

    RackComponent customization and releasing 1.14.0-beta.0

--- projects/ng-porcelain/src/lib/rack/rack/rack.component.ts
+++ projects/ng-porcelain/src/lib/rack/rack/rack.component.ts
@@ -60,6 +60,15 @@ export class RackComponent<TItemType extends any = any> implements OnInit {
 	@Input() valueProp: string = 'value';
 
 	/**
+	 * Active label input
+	 */
+	@Input() ActiveLabelHeader: string = '';
+	/**
+	 * Inactive label input
+	 */
+	@Input() InactiveLabelHeader: string = '';
+
+	/**
 	 * activeItems change emitter; emits changes to the activeItems property to parent scopes
 	 */
 	@Output() activeItemsChange = new EventEmitter<TItemType[]>();
