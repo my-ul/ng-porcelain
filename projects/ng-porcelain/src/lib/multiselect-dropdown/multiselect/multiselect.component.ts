00929fdbd55fb8558fa8e620b4b7ed819d5b84a4
Parameshwara Rao <83346@global.ul.com>
2021-04-23 01:08:42 +0530

    update to 9.0.9 version and porcelain multi select and insert functionality

--- projects/ng-porcelain/src/lib/multiselect-dropdown/multiselect/multiselect.component.ts
+++ projects/ng-porcelain/src/lib/multiselect-dropdown/multiselect/multiselect.component.ts
@@ -13,55 +13,48 @@ export class MultiSelectComponent implements OnInit {
 	readonly faAngleDown: IconDefinition = faAngleDown;
 	isOpen: boolean = false;
 
-	//send the user selected value
-
+	/**
+	 * sends the selected values in a single string separated by comma
+	 * */
 	@Output() public userEnteredInputBoxValue: EventEmitter<string> = new EventEmitter<string>();
-	@Input() public selectedLabel: string;
-	@Input() public multiselectItems: any[];
+
+	/**
+	 * sends only  selected Array to user
+	 *
+	 * */
+	@Output() public SelectedArrayItems: EventEmitter<Array<any>> = new EventEmitter<Array<any>>();
+
+	/**
+	 * Default Label string displayed
+	 * */
+	@Input() public selectedLabel: string = '';
+
+	/**
+	 * Displays placeholder value by default
+	 *
+	 * */
+	@Input() public selectedLabelPlaceholder: string = 'select';
 
 	/*{ id: 1, name: 'apple', isSelected: false },
 		{ id: 2, name: 'banana', isSelected: false }  Data format for multiselect
 		*/
 
+	/**
+	 * The Input List item to be show. NOTE VERY IMPORTANT!!
+	 * The input should Array of objects with A MANDATORY @field isSelected
+	 * like [{name:something, isSelected:false}]
+	 * */
 	@Input() public listItems: any[] = [];
 
-	public activeItems: any[] = [];
-	public inActiveItems: any[] = [];
+	/**
+	 * The label Prop to be displayed. By default set to name
+	 */
+	@Input() public LabelProp: string = 'name';
+
 	public focusToggle: boolean = false;
 	constructor(public eRef: ElementRef) {}
 
-	getValues(obj) {
-		return Object.values(obj);
-	}
-
-	onItemsSelection(inputItem: any) {
-		let key = inputItem.isSelected
-			? this.activeItems.findIndex(obj => obj.id == inputItem.id)
-			: this.listItems.findIndex(obj => obj.id == inputItem.id);
-		if (inputItem.isSelected) {
-			this.activeItems[key].isSelected = !this.activeItems[key].isSelected;
-		} else {
-			this.listItems[key].isSelected = !this.listItems[key].isSelected;
-		}
-		if (inputItem.isSelected) {
-			this.activeItems.push(this.listItems[key]);
-			this.listItems.splice(key, 1);
-		} else {
-			this.listItems.push(inputItem);
-			this.activeItems.splice(key, 1);
-		}
-		this.selectedLabel = this.getSelectedItems();
-		this.selectedLabel == '' ? (this.selectedLabel = 'Select') : '';
-	}
-
-	getSelectedItems(): string {
-		var items = this.activeItems.map(item => item.name);
-		let selectedValues = items.length > 1 ? items.join(', ') : items.toString();
-		this.userEnteredInputBoxValue.emit(selectedValues);
-		return selectedValues;
-	}
-
-	toggleOpen() {
+	public toggleOpen() {
 		if (this.isOpen) {
 			this.close();
 		} else {
@@ -69,7 +62,7 @@ export class MultiSelectComponent implements OnInit {
 		}
 	}
 
-	private open(): this {
+	public open(): this {
 		this.isOpen = true;
 		return this;
 	}
@@ -77,10 +70,57 @@ export class MultiSelectComponent implements OnInit {
 	/**
 	 * Closes the dropdown
 	 */
-	private close(): this {
+	public close(): this {
 		this.isOpen = false;
 		return this;
 	}
 
-	ngOnInit(): void {}
+	public ngOnInit(): void {}
+
+	/**
+	 *
+	 * Display no list separation if all are selected or none are selected
+	 * @param event
+	 * @param index
+	 */
+
+	public allOrNoneSelected(): boolean {
+		var selectedLength = this.listItems.filter(x => x.isSelected);
+		var unselectedLength = this.listItems.filter(x => !x.isSelected);
+		if (selectedLength.length != 0 && unselectedLength.length != 0) {
+			return true;
+		}
+		return false;
+	}
+
+	/**
+	 *
+	 * On user selection emit array and form the string
+	 * @param event
+	 * @param index
+	 */
+	public onUserselection(event: any, index): void {
+		var SelectedItemsArray = this.listItems.map(x => x).filter(x => x.isSelected);
+		this.SelectedArrayItems.emit(SelectedItemsArray);
+		//emit selected values
+		var selectedValueprops = this.listItems.filter(x => x.isSelected).map(x => x[this.LabelProp]);
+
+		let selectedValues =
+			selectedValueprops.length > 1
+				? selectedValueprops.join(', ')
+				: selectedValueprops.toString();
+		this.userEnteredInputBoxValue.emit(selectedValues);
+		//display values
+		this.selectedLabel = selectedValues == '' ? this.selectedLabelPlaceholder : selectedValues;
+	}
+
+	/**
+	 * reset and uncheck boxes
+	 * */
+	public resetFunctionality(): void {
+		this.listItems.forEach(x => {
+			x.isSelected = false;
+		});
+		this.selectedLabel = this.selectedLabelPlaceholder;
+	}
 }
