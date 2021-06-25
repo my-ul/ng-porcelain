6d3bfca8debf75a1f092380ba0525af4fec22896
S Shrinivas <85518@global.ul.com>
2021-06-25 18:59:41 +0530

    Code Review Fix

--- projects/ng-porcelain/src/lib/lists/list/list.component.ts
+++ projects/ng-porcelain/src/lib/lists/list/list.component.ts
@@ -6,9 +6,6 @@ import { Component, OnInit, HostBinding, Input } from '@angular/core';
 	styleUrls: ['./list.component.scss']
 })
 export class ListComponent implements OnInit {
-	// Question to ask since the Css is being applied to an inner element applying style to host will it
-	//overide. or can we use something like an ngClass to the toggle the classes
-
 	@HostBinding('class.list--as-table')
 	@Input()
 	isTableView: boolean = false;
