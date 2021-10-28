56f9c177718e93920804763192c0aab17b87178c
Brad Kovach <42781@global.ul.com>
2021-10-28 16:44:13 -0600

    Improve ToLocaleString pipe to use DateTimeFormat options and NumberFormatOptions Whitespace on tools run files

--- projects/ng-porcelain/src/lib/pipes/toLocaleString/to-locale-string.pipe.ts
+++ projects/ng-porcelain/src/lib/pipes/toLocaleString/to-locale-string.pipe.ts
@@ -14,14 +14,12 @@ export class ToLocaleStringPipe implements PipeTransform {
 	transform(
 		value: any,
 		locale: string = defaultLocale,
-		options: Intl.DateTimeFormatOptions = defaultDateTimeFormatOptions
+		options: Intl.DateTimeFormatOptions | Intl.NumberFormatOptions = defaultDateTimeFormatOptions
 	): any {
-		locale = locale || navigator.language || 'en-US';
-
 		if (typeof value === 'number') {
-			return value.toLocaleString(locale, options);
+			return value.toLocaleString(locale, options as Intl.NumberFormatOptions);
 		} else if (value instanceof Date) {
-			return value.toLocaleDateString(locale, options);
+			return value.toLocaleString(locale, options as Intl.DateTimeFormatOptions);
 		} else {
 			return value;
 		}
