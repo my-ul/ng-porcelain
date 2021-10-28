b5bc09e10cefb0c4c39df59701ac232256aba93a
Brad Kovach <42781@global.ul.com>
2021-10-28 16:34:41 -0600

    Adjust toLocaleString pipe to include ability to pass DateTimeFormatOptions.

--- projects/ng-porcelain/src/lib/pipes/toLocaleString/to-locale-string.pipe.ts
+++ projects/ng-porcelain/src/lib/pipes/toLocaleString/to-locale-string.pipe.ts
@@ -1,16 +1,27 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
+export const defaultLocale = navigator.language || 'en-us';
+export const defaultDateTimeFormatOptions = {
+	month: 'long',
+	day: 'numeric',
+	year: 'numeric'
+};
+
 @Pipe({
 	name: 'toLocaleString'
 })
 export class ToLocaleStringPipe implements PipeTransform {
-	transform(value: any, locale?: string): any {
+	transform(
+		value: any,
+		locale: string = defaultLocale,
+		options: Intl.DateTimeFormatOptions = defaultDateTimeFormatOptions
+	): any {
 		locale = locale || navigator.language || 'en-US';
 
 		if (typeof value === 'number') {
-			return value.toLocaleString(locale);
+			return value.toLocaleString(locale, options);
 		} else if (value instanceof Date) {
-			return value.toLocaleString(locale);
+			return value.toLocaleDateString(locale, options);
 		} else {
 			return value;
 		}
