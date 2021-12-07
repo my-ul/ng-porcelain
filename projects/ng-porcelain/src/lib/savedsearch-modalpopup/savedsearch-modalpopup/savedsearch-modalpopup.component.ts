c24ab4a1c2bf80a4ff8ffe94496db8be68b9dfaf
S Shrinivas <85518@global.ul.com>
2021-12-07 19:52:18 +0530

    Modifying the SaveSearch to use content projection

--- projects/ng-porcelain/src/lib/savedsearch-modalpopup/savedsearch-modalpopup/savedsearch-modalpopup.component.ts
+++ projects/ng-porcelain/src/lib/savedsearch-modalpopup/savedsearch-modalpopup/savedsearch-modalpopup.component.ts
@@ -9,10 +9,7 @@ export class SavedsearchModalpopupComponent implements OnInit {
 	constructor() {}
 	@Output() onCheckBoxClick = new EventEmitter<string>();
 	@Output() onModalClose = new EventEmitter<string>();
-	@Input() quickTipHeader: string = 'Quick Tip';
-	@Input() quickTipContent: string =
-		'Save your searches for quick access with the Save Search button in certain applications. You can access your saved searches anywhere in the portal by clicking on this Saved Searches link.';
-	@Input() checkBoxContent: string = "Don't show this again";
+
 	ngOnInit(): void {}
 
 	onCheckboxClick() {
