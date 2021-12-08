d7794bdaa5061fd97344b65bbad8041e1f7abd24
Parameshwara Rao <83346@global.ul.com>
2021-12-08 20:08:38 +0530

    sample dropdown Implementation

--- projects/ng-porcelain/src/lib/dropdown-system/dropdown-system.module.ts
+++ projects/ng-porcelain/src/lib/dropdown-system/dropdown-system.module.ts
@@ -1,15 +1,26 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+//import directives
 import { DropdownSelectComponent } from './dropdown-select/dropdown-select.component';
 import { DropdownSelectOptionComponent } from './dropdown-select-option/dropdown-select-option.component';
 import { DropdownSelectedTemplateComponent } from './dropdown-selected-template/dropdown-selected-template.component';
 
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { PipesModule } from '../pipes/pipes.module';
+
+export const DROPDOWNSYSTEM_DIRECTIVES = [
+	DropdownSelectComponent,
+	DropdownSelectOptionComponent,
+	DropdownSelectedTemplateComponent
+];
+
+export const DROPDOWNSYSTEM_IMPORTS = [CommonModule, FormsModule, PipesModule, FontAwesomeModule];
+
 @NgModule({
-	declarations: [
-		DropdownSelectComponent,
-		DropdownSelectOptionComponent,
-		DropdownSelectedTemplateComponent
-	],
-	imports: [CommonModule]
+	declarations: DROPDOWNSYSTEM_DIRECTIVES,
+	imports: DROPDOWNSYSTEM_IMPORTS,
+	exports: DROPDOWNSYSTEM_DIRECTIVES
 })
 export class DropdownSystemModule {}
