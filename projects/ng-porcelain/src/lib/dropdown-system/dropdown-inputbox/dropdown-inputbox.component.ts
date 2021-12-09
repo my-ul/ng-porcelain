5c6fa604008d166df2e4cc15fae86290504a8234
Parameshwara Rao <83346@global.ul.com>
2021-12-09 21:35:48 +0530

    dropdown input box initialize

---
+++ projects/ng-porcelain/src/lib/dropdown-system/dropdown-inputbox/dropdown-inputbox.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+	selector: 'porcelain-dropdown-inputbox',
+	templateUrl: './dropdown-inputbox.component.html',
+	styleUrls: ['./dropdown-inputbox.component.scss']
+})
+export class DropdownInputboxComponent implements OnInit {
+	constructor() {}
+
+	ngOnInit(): void {}
+}
