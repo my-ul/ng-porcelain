6bc8b493e5e48f881ffbf031ef8775752a90f16c
Brad Kovach <bradkovach@gmail.com>
2024-03-25 10:56:42 -0600

    add link-component to public api

--- projects/ng-porcelain/src/public_api.ts
+++ projects/ng-porcelain/src/public_api.ts
@@ -6,6 +6,7 @@ export { DateRefinerComponent } from './lib/date-refiner/date-refiner/date-refin
 
 export { FooterModule } from './lib/footer/footer.module';
 export { FooterComponent } from './lib/footer/footer/footer.component';
+export { LinkDirective } from './lib/footer/link.directive';
 
 export { RefinersModule } from './lib/refiners/refiners.module';
 export { RefinersComponent } from './lib/refiners/refiners/refiners.component';
@@ -13,10 +14,10 @@ export { RefinersComponent } from './lib/refiners/refiners/refiners.component';
 export { SearchInputModule as LegacySearchInputModule } from './lib/search-input/search-input.module';
 export { SearchInputComponent as LegacySearchInputComponent } from './lib/search-input/search-input/search-input.component';
 
-export { SimpleRefinerModule } from './lib/simple-refiner/simple-refiner.module';
 export { SimpleRadioRefinerModule } from './lib/simple-radio-refiner/simple-radio-refiner.module';
-export { SimpleRefinerComponent } from './lib/simple-refiner/simple-refiner/simple-refiner.component';
 export { SimpleRadioRefinerComponent } from './lib/simple-radio-refiner/simple-radio-refiner/simple-radio-refiner.component';
+export { SimpleRefinerModule } from './lib/simple-refiner/simple-refiner.module';
+export { SimpleRefinerComponent } from './lib/simple-refiner/simple-refiner/simple-refiner.component';
 export { SpinnerModule } from './lib/spinner/spinner.module';
 export { SpinnerComponent } from './lib/spinner/spinner/spinner.component';
 
@@ -49,24 +50,30 @@ export { SimpleOption } from './lib/shared/types/Options/SimpleOption';
 export { SimpleOptions } from './lib/shared/types/Options/SimpleOptions';
 
 // Refiner definitions
-export { BaseRefiner, BaseRefinerDefinition } from './lib/shared/types/Refiners/BaseRefinerDefinition';
-export { DateRefiner, DateRefinerDefinition } from './lib/shared/types/Refiners/DateRefinerDefinition';
+export {
+	BaseRefiner,
+	BaseRefinerDefinition,
+} from './lib/shared/types/Refiners/BaseRefinerDefinition';
+export {
+	DateRefiner,
+	DateRefinerDefinition,
+} from './lib/shared/types/Refiners/DateRefinerDefinition';
 export {
 	IBaseRefiner,
-	IBaseRefinerDefinition
+	IBaseRefinerDefinition,
 } from './lib/shared/types/Refiners/IBaseRefinerDefinition';
 export {
 	IDateRefiner,
-	IDateRefinerDefinition
+	IDateRefinerDefinition,
 } from './lib/shared/types/Refiners/IDateRefinerDefinition';
 export {
 	ISimpleRefiner,
-	ISimpleRefinerDefinition
+	ISimpleRefinerDefinition,
 } from './lib/shared/types/Refiners/ISimpleRefinerDefinition';
 export { RefinerType } from './lib/shared/types/Refiners/RefinerType';
 export {
 	SimpleRefiner,
-	SimpleRefinerDefinition
+	SimpleRefinerDefinition,
 } from './lib/shared/types/Refiners/SimpleRefinerDefinition';
 
 // Values
@@ -74,23 +81,26 @@ export { DateRefinerValue } from './lib/shared/types/Values/DateRefinerValue';
 export { IDateRefinerValue } from './lib/shared/types/Values/IDateRefinerValue';
 export { OptionRefinerValue } from './lib/shared/types/Values/OptionRefinerValue';
 export { RefinerValue } from './lib/shared/types/Values/RefinerValue';
-export { SortDirection, SortTuple } from './lib/shared/types/Values/Sortoptions';
+export {
+	SortDirection,
+	SortTuple,
+} from './lib/shared/types/Values/Sortoptions';
 
 // Shared utilities
-export { i18nDateOptions } from './lib/shared/utilities/i18n/i18nDateOptions/i18nDateOptions';
-export { fromEntries } from './lib/shared/utilities/reducers/fromEntries/fromEntries';
 export { clamp } from './lib/shared/utilities/arrays/clamp';
 export { moveItem } from './lib/shared/utilities/arrays/moveItem';
+export { i18nDateOptions } from './lib/shared/utilities/i18n/i18nDateOptions/i18nDateOptions';
+export { fromEntries } from './lib/shared/utilities/reducers/fromEntries/fromEntries';
 
 // Pipes module
-export { PipesModule } from './lib/pipes/pipes.module';
 export { CeilPipe } from './lib/pipes/ceil/ceil.pipe';
+export { FilterItem, FilterPipe } from './lib/pipes/filter/filter.pipe';
 export { FloorPipe } from './lib/pipes/floor/floor.pipe';
+export { HighlightPipe } from './lib/pipes/highlight/highlight.pipe';
+export { PipesModule } from './lib/pipes/pipes.module';
 export { RoundPipe } from './lib/pipes/round/round.pipe';
 export { SprintfPipe } from './lib/pipes/sprintf/sprintf.pipe';
 export { ToLocaleStringPipe } from './lib/pipes/toLocaleString/to-locale-string.pipe';
-export { FilterPipe, FilterItem } from './lib/pipes/filter/filter.pipe';
-export { HighlightPipe } from './lib/pipes/highlight/highlight.pipe';
 
 // Rack module
 export { RackModule } from './lib/rack/rack.module';
@@ -112,54 +122,57 @@ export {
 	ISetChildLoadedResponse,
 	ISuccessMessage,
 	ISuccessResponse,
-	MessageType
+	MessageType,
 } from './lib/services/frame-guest/frame-guest.service';
 export { FrameHostService } from './lib/services/frame-host/frame-host.service';
 export {
 	GoogleAnalyticsEvent,
 	GoogleAnalyticsService,
-	IGoogleAnalytics
+	IGoogleAnalytics,
 } from './lib/services/google-analytics/google-analytics.service';
 export {
-	Translations,
 	TranslationMap,
-	TranslationService
+	TranslationService,
+	Translations,
 } from './lib/services/translation/translation.service';
 export { WindowService } from './lib/services/window/window.service';
 
 // Inputs module
 export { InputsModule } from './lib/inputs/inputs.module';
 export { PasswordInputComponent } from './lib/inputs/password-input/password-input.component';
-export { TextInputComponent } from './lib/inputs/text-input/text-input.component';
 export { SearchInputComponent } from './lib/inputs/search-input/search-input.component';
+export { TextInputComponent } from './lib/inputs/text-input/text-input.component';
 
 // Lists module
-export { ListsModule } from './lib/lists/lists.module';
 export {
 	DynamicColumn,
 	DynamicColumnType,
+	DynamicHeaderComponent,
 	DynamicSearchQuery,
-	DynamicHeaderComponent
 } from './lib/lists/dynamic-header/dynamic-header.component';
-export { ListComponent } from './lib/lists/list/list.component';
 export { ListBodyComponent } from './lib/lists/list-body/list-body.component';
-export { ListHeaderComponent } from './lib/lists/list-header/list-header.component';
 export { ListHeaderCellComponent } from './lib/lists/list-header-cell/list-header-cell.component';
-export { ListItemComponent } from './lib/lists/list-item/list-item.component';
+export { ListHeaderComponent } from './lib/lists/list-header/list-header.component';
 export { ListItemCellComponent } from './lib/lists/list-item-cell/list-item-cell.component';
-export { SearchHeaderComponent, SearchTuple } from './lib/lists/search-header/search-header.component';
+export { ListItemComponent } from './lib/lists/list-item/list-item.component';
+export { ListComponent } from './lib/lists/list/list.component';
+export { ListsModule } from './lib/lists/lists.module';
+export {
+	SearchHeaderComponent,
+	SearchTuple,
+} from './lib/lists/search-header/search-header.component';
 export { SortHeaderComponent } from './lib/lists/sort-header/sort-header.component';
 export { TextHeaderComponent } from './lib/lists/text-header/text-header.component';
 
 // Toolbar module
-export { ToolbarModule } from './lib/toolbar/toolbar.module';
-export { ToolbarComponent } from './lib/toolbar/toolbar/toolbar.component';
 export { ToolbarButtonComponent } from './lib/toolbar/toolbar-button/toolbar-button.component';
 export { ToolbarCellComponent } from './lib/toolbar/toolbar-cell/toolbar-cell.component';
 export { ToolbarOptionComponent } from './lib/toolbar/toolbar-option/toolbar-option.component';
 export { ToolbarSelectComponent } from './lib/toolbar/toolbar-select/toolbar-select.component';
 export { ToolbarSelectedTemplateComponent } from './lib/toolbar/toolbar-selected-template/toolbar-selected-template.component';
 export { ToolbarTextComponent } from './lib/toolbar/toolbar-text/toolbar-text.component';
+export { ToolbarModule } from './lib/toolbar/toolbar.module';
+export { ToolbarComponent } from './lib/toolbar/toolbar/toolbar.component';
 export { ToolbarsComponent } from './lib/toolbar/toolbars/toolbars.component';
 
 // Combobox Module
@@ -167,22 +180,25 @@ export { ComboboxModule } from './lib/combobox/combobox.module';
 export { ComboboxComponent } from './lib/combobox/combobox/combobox.component';
 
 // Breadcrumb Modules
+export { BreadcrumbItemComponent } from './lib/breadcrumb/breadcrumb-item/breadcrumb-item.component';
 export { BreadcrumbModule } from './lib/breadcrumb/breadcrumb.module';
 export { BreadcrumbsComponent } from './lib/breadcrumb/breadcrumbs/breadcrumbs.component';
-export { BreadcrumbItemComponent } from './lib/breadcrumb/breadcrumb-item/breadcrumb-item.component';
 
 // Expando Module
-export { ExpandoModule } from './lib/expando/expando.module';
-export { ExpandoComponent, ExpandoIconPosition } from './lib/expando/expando/expando.component';
 export { ExpandoBodyComponent } from './lib/expando/expando-body/expando-body.component';
 export { ExpandoHeaderComponent } from './lib/expando/expando-header/expando-header.component';
+export { ExpandoModule } from './lib/expando/expando.module';
+export {
+	ExpandoComponent,
+	ExpandoIconPosition,
+} from './lib/expando/expando/expando.component';
 
 // Skeleton Module
-export { SkeletonsModule } from './lib/skeletons/skeletons.module';
 export { SkeletonBlockComponent } from './lib/skeletons/block/block.component';
-export { SkeletonWordComponent } from './lib/skeletons/word/word.component';
 export { SkeletonLineComponent } from './lib/skeletons/line/line.component';
 export { SkeletonParagraphComponent } from './lib/skeletons/paragraph/paragraph.component';
+export { SkeletonsModule } from './lib/skeletons/skeletons.module';
+export { SkeletonWordComponent } from './lib/skeletons/word/word.component';
 
 // toSimpleOptionDictionary utility
 export { toSimpleOptionDictionary } from './lib/shared/utilities/toSimpleOptionDictionary';
@@ -192,8 +208,8 @@ export { SearchRefinerModule } from './lib/search-refiner/search-refiner.module'
 export { SearchRefinerComponent } from './lib/search-refiner/search-refiner/search-refiner.component';
 
 //DatePicker module
-export { datePickerComponent } from './lib/date-picker/date-picker/date-picker.component';
 export { DatePickerModule } from './lib/date-picker/date-picker.module';
+export { datePickerComponent } from './lib/date-picker/date-picker/date-picker.component';
 
 export { MultiSelectModule } from './lib/multiselect-dropdown/multiselect.module';
 export { MultiSelectComponent } from './lib/multiselect-dropdown/multiselect/multiselect.component';
@@ -206,21 +222,21 @@ export { CheckboxModule } from './lib/checkbox/checkbox.module';
 export { CheckboxComponent } from './lib/checkbox/checkbox/checkbox.component';
 
 //tabeview module
-export { TableviewModule } from './lib/tableview-module/tableview-module.module';
-export { TableviewListComponent } from './lib/tableview-module/tableview-list/tableview-list.component';
-export { TableviewListBodyComponent } from './lib/tableview-module/tableview-list-body/tableview-list-body.component';
-export { TableviewListItemComponent } from './lib/tableview-module/tableview-list-item/tableview-list-item.component';
-export { TableviewListItemCellComponent } from './lib/tableview-module/tableview-list-item-cell/tableview-list-item-cell.component';
-export { TableviewHeaderComponent } from './lib/tableview-module/tableview-header/tableview-header.component';
-export { TableviewHeaderItemComponent } from './lib/tableview-module/tableview-header-item/tableview-header-item.component';
-export { TableviewTextHeaderComponent } from './lib/tableview-module/tableview-text-header/tableview-text-header.component';
-export { TableviewSortHeaderComponent } from './lib/tableview-module/tableview-sort-header/tableview-sort-header.component';
-export { TableviewSearchSortHeaderComponent } from './lib/tableview-module/tableview-searchSortHeader/tableview-searchSortHeader.component';
 export { BackgroundColorDirective } from './lib/tableview-module/color-directive/background-color.directive';
+export { TableviewHeaderItemComponent } from './lib/tableview-module/tableview-header-item/tableview-header-item.component';
+export { TableviewHeaderComponent } from './lib/tableview-module/tableview-header/tableview-header.component';
+export { TableviewListBodyComponent } from './lib/tableview-module/tableview-list-body/tableview-list-body.component';
+export { TableviewListItemCellComponent } from './lib/tableview-module/tableview-list-item-cell/tableview-list-item-cell.component';
+export { TableviewListItemComponent } from './lib/tableview-module/tableview-list-item/tableview-list-item.component';
+export { TableviewListComponent } from './lib/tableview-module/tableview-list/tableview-list.component';
+export { TableviewModule } from './lib/tableview-module/tableview-module.module';
+export { TableviewSearchSortHeaderComponent } from './lib/tableview-module/tableview-searchSortHeader/tableview-searchSortHeader.component';
+export { TableviewSortHeaderComponent } from './lib/tableview-module/tableview-sort-header/tableview-sort-header.component';
+export { TableviewTextHeaderComponent } from './lib/tableview-module/tableview-text-header/tableview-text-header.component';
 
 //dropdown module
-export { DropdownSystemModule } from './lib/dropdown-system/dropdown-system.module';
 export { DropdownInputboxComponent } from './lib/dropdown-system/dropdown-inputbox/dropdown-inputbox.component';
-export { DropdownSelectComponent } from './lib/dropdown-system/dropdown-select/dropdown-select.component';
 export { DropdownSelectOptionComponent } from './lib/dropdown-system/dropdown-select-option/dropdown-select-option.component';
+export { DropdownSelectComponent } from './lib/dropdown-system/dropdown-select/dropdown-select.component';
 export { DropdownSelectedTemplateComponent } from './lib/dropdown-system/dropdown-selected-template/dropdown-selected-template.component';
+export { DropdownSystemModule } from './lib/dropdown-system/dropdown-system.module';
